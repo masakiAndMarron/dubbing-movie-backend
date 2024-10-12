8655c24fc97c1e6e23344a09980f09c6532fe7b0
Msaki <msakidummy@gmail.com>
2024-10-12 14:38:02 +0900

    first commit

---
+++ src/main/java/com/example/dubbing/DubbingMovieBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.dubbing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DubbingMovieBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DubbingMovieBackendApplication.class, args);
+	}
+
+}
